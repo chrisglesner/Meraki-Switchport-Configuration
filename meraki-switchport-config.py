b2df838091db8a2fd505ec4a990be40459665bef
chrisglesner <129309881+chrisglesner@users.noreply.github.com>
2023-03-31 15:49:10 -0500

    Update meraki-switchport-config.py

--- meraki-switchport-config.py
+++ meraki-switchport-config.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     for switchport in [*range(1,49)]: #Change Range Command for Your Desired Interfaces
         response = dashboard.switch.updateDeviceSwitchPort(serial='YOUR SERIAL NUMBER OF SWITCH', portId=switchport, name='Data', type='access', vlan=100)
 
-        with open('DIRECTORY WHERsE YOUR LOGGING FILE EXISTS', 'a') as my_file:
+        with open('DIRECTORY WHERE YOUR LOGGING FILE EXISTS', 'a') as my_file:
             data = json.dumps(response, indent=4)
             my_file.write(data)
