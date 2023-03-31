5a73c93013b28b4a29b856da8830f07bb2d280f1
chrisglesner <129309881+chrisglesner@users.noreply.github.com>
2023-03-31 15:27:49 -0500

    Add files via upload

---
+++ meraki-switchport-config.py
@@ -0,0 +1,13 @@
+import meraki
+import json
+
+if __name__ == '__main__':
+    api_key = 'YOUR API KEY'
+    dashboard = meraki.DashboardAPI(api_key)
+  
+    for switchport in [*range(1,49)]: #Change Range Command for Your Desired Interfaces
+        response = dashboard.switch.updateDeviceSwitchPort(serial='YOUR SERIAL NUMBER OF SWITCH', portId=switchport, name='Data', type='access', vlan=100)
+
+        with open('DIRECTORY WHERsE YOUR LOGGING FILE EXISTS', 'a') as my_file:
+            data = json.dumps(response, indent=4)
+            my_file.write(data)
